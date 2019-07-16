259db3436029644b1ba57bfb73ceaba60fb304eb
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-16 11:53:39 +0200

    Added __init__

---
+++ pp_role_mining/__init__.py
@@ -0,0 +1,8 @@
+from pp_role_mining import evaluation, privacyPreserving, SN_Tools, Utilities
+
+__version__ = '0.0.1'
+__doc__ = "Privacy Aware Role Mining in PM4Py"
+__author__ = 'Majid Rafiei'
+__author_email__ = 'majid.rafiei@pads.rwth-aachen.de'
+__maintainer__ = 'Majid Rafiei'
+__maintainer_email__ = "majid.rafiei@pads.rwth-aachen.de"
