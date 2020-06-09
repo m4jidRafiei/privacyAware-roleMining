94d3b3ef542db81b73303f473889c75740a505c7
Majid <m4jid.rafiei@gmail.com>
2020-06-09 15:54:06 +0200

    readme updated

--- pp_role_mining/__init__.py
+++ pp_role_mining/__init__.py
@@ -1,7 +1,7 @@
 from pp_role_mining import evaluation, privacyPreserving, SN_Tools, Utilities
 
 __version__ = '0.0.10'
-__doc__ = "Privacy Aware Role Mining in PM4Py"
+__doc__ = "Privacy Aware Role Mining in Process mining"
 __author__ = 'Majid Rafiei'
 __author_email__ = 'majid.rafiei@pads.rwth-aachen.de'
 __maintainer__ = 'Majid Rafiei'
